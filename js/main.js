c66d785d1c7973c9def58357790b19e7a490c8b7
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-08 18:10:08 +0600

    testimonial section finished

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const swiper = new Swiper(".swiper", {
   spaceBetween: 20,
   breakpoints: {
     320: { slidesPerView: 1 },
-    768: { slidesPerView: 2 },
-    960: { slidesPerView : 3},
+    960: { slidesPerView: 2 },
+    1200: { slidesPerView: 3 },
   },
 });
