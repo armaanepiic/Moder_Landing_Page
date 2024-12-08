5167750da5f13ae04b85446dbcc72f2b45c4e7f3
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-08 18:01:35 +0600

    slider

--- js/main.js
+++ js/main.js
@@ -3,3 +3,20 @@ const navMenu = document.getElementById("nav_manu");
 navTriggerBtn.addEventListener('click', () => {
     navMenu.classList.toggle('nav-is-open');
 });
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // breakpoints
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    960: { slidesPerView : 3},
+  },
+});
